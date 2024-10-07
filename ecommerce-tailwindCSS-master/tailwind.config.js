de960019a7df169f299c2ba2137f956a84887e27
Indhu0525 <indu01401@hmail.com>
2024-10-07 11:41:48 +0530

    excomerce website

---
+++ ecommerce-tailwindCSS-master/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Add your paths here
+  ],
+  theme: {
+    extend: {
+      visibility: ["group-hover"],
+    },
+  },
+  plugins: [],
+}
